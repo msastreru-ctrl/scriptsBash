a63f74e4112809f7a835dca478150bccd3a354ef
Miquel <msastreru@esliceu.net>
2026-04-14 16:50:56 +0200

    segon script

---
+++ 002_operacions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#variables són "paraules" que contenen un valor
+NOM="Miquel"
+#read serveix per llegir informació del taclat
+read -p "introdueix un nom: " NOM 
+
+
+echo "El teu nom és ${NOM}"
