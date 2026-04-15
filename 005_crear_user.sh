a3ee99c42cb08c9289aec54540640f0f9f04d545
Miquel <msastreru@esliceu.net>
2026-04-15 16:09:01 +0200

    video 12

--- 005_crear_user.sh
+++ 005_crear_user.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+function usage(){
+    echo "Usage ${O}"
+    echo 
+    echo "Has de ser ROOT"
+    echo "l'usuari no es pot repetir"
+    exit 1
+}
 
 #script per crear un usuari
 
 #primer comprovam si som l'usuari root
 if [[ ${UID} -ne 0 ]]
 then
-    echo "Usage ${O}"
-    echo 
-    echo "Has de ser ROOT"
-    exit 1
+    usage 
 fi
 
 read -p "introdueix el nom d'usuari: " USER_NAME
